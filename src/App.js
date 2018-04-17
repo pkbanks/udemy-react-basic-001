7fa9d9e33e6d23ccf1fdc867c81b88cf2f2daebd
PK Banks <pkbanks12@gmail.com>
2018-04-17 14:02:56 -0400

    components basics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: "PK", age: 38 },
+      { name: "Carolina", age: 40 },
+      { name: "Nathaniel", age: 1 }
+    ]
+  }
+
+  switchNameHandler = ( newName ) => {
+    // console.log('was clicked');
+    this.setState({
+      persons: [
+        { name: newName, age: 38 },
+        { name: "Carolina", age: 40 },
+        { name: "Nathaniel", age: 1 }
+      ]
+    });
+  }
+
   render() {
-    return (
+    // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Hi, I\'m a react app!!!'));
+    return(
       <div className="App">
         <h1>Hi, I'm a react app</h1>
+        <button onClick={ this.switchNameHandler(this, "Paul K Banks" ) }>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={ this.state.persons[0].age } />
+        <Person
+          name={this.state.persons[1].name}
+          age={ this.state.persons[0].age }
+          click={ this.switchNameHandler } />
+        <Person
+          name={this.state.persons[2].name}
+          age={ this.state.persons[0].age } >
+            <h3>My Hobbies: Coffee</h3>
+        </Person>
       </div>
-    );
+    )
   }
 }
 
